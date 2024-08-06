d3beb8ae7a076dc10facd6063e237c5c55e93f0a
Hasankhan170 <hasankhanali0316@gmail.com>
2024-08-06 07:29:53 -0700

    image get work

--- register.js
+++ register.js
@@ -1,5 +1,7 @@
 import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
-import { auth } from "./config.js";
+import { auth ,storage ,ref } from "./config.js"; // Correct import statement
+import { uploadBytes , getDownloadURL } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-storage.js";
+
 
 
 const registerForm = document.querySelector('#registerForm')
@@ -7,28 +9,73 @@ const email = document.querySelector('#email')
 const password = document.querySelector('#password')
 const firstName = document.querySelector('#first-name')
 const lastName = document.querySelector('#last-name')
+const chooseFile = document.querySelector('#choose-file')
 
 
 registerForm.addEventListener('submit' , (e)=>{
     e.preventDefault()
-    createUserWithEmailAndPassword(auth, email.value, password.value,firstName.value,lastName.value)
+
+    // get the file from the input 
+    const file = chooseFile.files[0]
+    
+    // Create user with email and password
+    createUserWithEmailAndPassword(auth, email.value, password.value)
     .then((userCredential) => {
       const user = userCredential.user;
       console.log(user);
+      console.log(file);
       
-      setTimeout(()=>{
-        alert('register successfully!')
-        window.location = "./login.html"
-      },1000)
 
-      email.value = ""
-      password.value = ""
-      firstName.value = ""
-      lastName.value = ""
+      // Handle file upload if a file was chosen
+
+      if(file){
+        // create a reference to the storage bucket
+        const storageRef = ref(storage, file.name) // File path in storage
+        uploadBytes(storageRef, file).then((snapshot) => {
+            console.log('Uploaded a blob or file!');
+          });
+        
+          // Update user profile with name and image url
+
+      getDownloadURL(storageRef)
+      .then((url) => {
+        console.log(url);
+      })
+      .catch((error)=>{
+        console.log(error);
+        alert('Error getting download URL');
+      })
+
+
+
+      
+      
+
+      then(()=>{
+        setTimeout(()=>{
+            alert('register successfully!')
+            window.location = "./login.html"
+          },1000)
+      })
+    }
+
 
     })
     .catch((error) => {
       const errorMessage = error.message;
       console.log(errorMessage);
     });
+
+
+    // clear inputs fields
+    email.value = ""
+    password.value = ""
+    firstName.value = ""
+    lastName.value = ""
+    chooseFile.value = ""
 })
+
+
+
+
+
