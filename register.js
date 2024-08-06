b3551ca6341bbadb71f0d1bc9e239dfccd98455a
Hasankhan170 <hasankhanali0316@gmail.com>
2024-08-06 06:09:45 -0700

    register page work

---
+++ register.js
@@ -0,0 +1,34 @@
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import { auth } from "./config.js";
+
+
+const loginForm = document.querySelector('#loginForm')
+const email = document.querySelector('#email')
+const password = document.querySelector('#password')
+const firstName = document.querySelector('#first-name')
+const lastName = document.querySelector('#last-name')
+
+
+loginForm.addEventListener('submit' , (e)=>{
+    e.preventDefault()
+    createUserWithEmailAndPassword(auth, email.value, password.value,firstName.value,lastName.value)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log(user);
+      
+      setTimeout(()=>{
+        alert('register successfully!')
+        window.location = "./login.html"
+      },1000)
+
+      email.value = ""
+      password.value = ""
+      firstName.value = ""
+      lastName.value = ""
+
+    })
+    .catch((error) => {
+      const errorMessage = error.message;
+      console.log(errorMessage);
+    });
+})
