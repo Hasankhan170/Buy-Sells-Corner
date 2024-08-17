36566c4cf19438fe3bc17684489c23cebac4a0ef
Hasankhan170 <hasankhanali0316@gmail.com>
2024-08-17 07:26:09 -0700

    working

--- singlepage.js
+++ singlepage.js
@@ -16,7 +16,41 @@ function renderGetData(){
     const products = JSON.parse(getProducts)
     products.forEach((item)=>{
         showIndexData.innerHTML += `
+        <div class="main-boxSingle mt-10">
+      <div class=".img-box-singlePage"> 
+        <img class="img-singlePage" src="${item.PoductImage}" alt="">
+      </div>
+
+      <div class="test-box-singlePage">
+
+        <div class="inner-main">
+          <div>
+            <h1>Rs ${item.price}</h1>
+          </div>
+          <div>
+            <p><i class="fa-solid fa-download"></i> <i class="fa-regular fa-heart"></i></p>
+          </div>
+        </div>
+
+        <p style="margin-top: 20px;">${item.title}</p>
+        <p style="margin-top: 5px;">${item.description}</p>
+
+        <div class="blue-box-main">
+          <div class="blue-box-1">
+            <img class="blue-box-img" src="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBwgHBgkIBwgKCgkLDRYPDQwMDRsUFRAWIB0iIiAdHx8kKDQsJCYxJx8fLT0tMTU3Ojo6Iys/RD84QzQ5OjcBCgoKDQwNGg8PGjclHyU3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3N//AABEIAJQA1gMBIgACEQEDEQH/xAAWAAEBAQAAAAAAAAAAAAAAAAAAAQf/xAAWEAEBAQAAAAAAAAAAAAAAAAAAARH/xAAVAQEBAAAAAAAAAAAAAAAAAAAAAf/EABQRAQAAAAAAAAAAAAAAAAAAAAD/2gAMAwEAAhEDEQA/AMREAAAAAAACgAqCCgAgAKJFAABRAFQAAFABBBUUAAAABQEAQUAAABFAQKAoAAAAAAACpFBEUUQAFAQBKQFRUBRAFAFAAEUBFQEUQBRFAAAAAAARUBUVAAAAAAAFQBQBQAEFQQAAVFABAXFRQQAUAEEVAFQAAAAFABAAVRFAEUAAAAQAAAAAFAAAAQVBAAAAAAAAAAFAFAAABAAAWAqAKgAigAAAIAIAAAAAAAAAAoAoAIAAAAAKACKAAAAIqCAAAAAAAAAAKIoAAAFAAAVAUAAAAAEEAAAAAAAAAAAAgQFAFAIACggCoAIAAAAIAoAIAAAAAoAAAAAAAoCgAP/Z" alt="">
+          </div>
+          <div class="blue-box-text">
+            <p>${item.name}</p>
+            <p>${item.number}</p>
+            <button id="call-btn">Add to Cart</button>
+            <button id="chat-btn">Buy Now</button>
+
+          </div>
+
+        </div>
+        <button id="add-to-card-btn">Add to cart</button>
         
+      </div>
         `
     })
 }
